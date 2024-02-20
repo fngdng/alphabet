b550df25c18d1ae6a612d2ef504b99ead61eac1f
fong <115392329+fngdng@users.noreply.github.com>
2024-02-20 16:35:11 +0700

    Update server.js
    first draft for server tracking and auto updating your bot status to website (you may need a host for your website and a domain name to search for it, we will make a server public source for bot-runner and a global server to check for every single bot status and author)

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-//not yet
+import axios from 'axios'
+import wss from 'wss'
+var key = ''
+
+(async () => {
+  const checkkeytrue = await axios.get("your server link/$key", (res,err) => {
+    if(err) {
+      console.log('wrong key, check ur server link may not correct or your key not eligible to login to the server!')
+    } else {
+      console.log(res.data)
+    }
+  })
+})()
