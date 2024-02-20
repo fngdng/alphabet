6bceb27304ab977bae0268602929a4e8dafb29f3
fong <115392329+fngdng@users.noreply.github.com>
2024-02-20 16:43:22 +0700

    Update check_update.js
    first draft for checkupdate, update later

--- check_update.js
+++ check_update.js
@@ -1,1 +1,13 @@
-//not yet
+import axios from 'axios'
+import fs from 'fs'
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var version = fs.readStream(__dirname('package.json'))
+
+async function checkforupdate() {
+  var globalpackage = await axios.get('https://raw.githubusercontent.com/fngdng/alphabet/main/package.json')
+  var globalversion = globalpackage.version
+  if(version != globalversion) {
+    console.log("ur bot version is not sync to global version, you may using outdated version or beta version, if u r using outdated version, please update to use latest perform of out works!")
+  }
+}
