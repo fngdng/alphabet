300474bcec54b3541e3196651b86947221a7ded5
fong <115392329+fngdng@users.noreply.github.com>
2024-02-20 16:24:25 +0700

    Update core_opt.js
    change userAgent

--- core_opt.js
+++ core_opt.js
@@ -286,7 +286,7 @@ class bot {
       updatePresence: false,
       listenEvents: true,
       forceLogin: true,
-      userAgent: "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Mobile Safari/537.36"
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0"
     });
     api.listenMqtt((err, event) => {
       if (err) return console.error(err);
